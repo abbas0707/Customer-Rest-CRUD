b7e641fe4b3c22f7b5dbf8d0e56fcc9887551974
abbas <abba0707@gmail.com>
2018-11-10 23:28:03 +1100

    First commit

---
+++ src/main/java/com/customer/rest/action/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package com.customer.rest.action.controller;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.customer.rest.action.domain.Customer;
+import com.customer.rest.action.service.CustomerService;
+
+@RestController
+public class CustomerController {
+	
+@Autowired
+private CustomerService customerSerice;
+private static Log logger = LogFactory.getLog(CustomerController.class);
+	/*
+	 * Method resturns list of customers from JSON store.
+	 * */
+	@RequestMapping("/listCustomers")
+	public List<Customer> getCustomers() throws Exception {	
+		logger.info("Request received for /listCustomers");
+		List<Customer> customers = customerSerice.findAllCustomers();
+		logger.info("Response for /listCustomers, Total Customers: "+customers.size());
+		return customers;
+		
+	}
+
+}
---
+++ src/main/java/com/customer/rest/action/exception/CustomExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.customer.rest.action.exception;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.customer.rest.action.domain.FaultDetail;
+ 
+@ControllerAdvice
+@RestController
+public class CustomExceptionHandler  extends ResponseEntityExceptionHandler {
+	private static Log logger = LogFactory.getLog(CustomExceptionHandler.class);
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<FaultDetail> handleAllExceptions(Exception ex, WebRequest request) {	 
+		logger.error("Exception occured for "+request.getDescription(false)+", "+ex.getMessage());
+		FaultDetail errorDetails = new FaultDetail(new Date(), ex.getMessage(),
+	      request.getDescription(false));		
+	  return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
